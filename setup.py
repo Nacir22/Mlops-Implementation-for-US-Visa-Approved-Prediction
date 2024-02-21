a2ad71d845abc17e2c5044cc62efaa90d4974f79
Nacir22 <boutra.nacir@gmail.com>
2024-02-21 18:29:00 +0100

    second update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version="0.0.0",
-    author="Bappy",
-    author_email="entbappy73@gmail.com",
+    author="Nacir",
+    author_email="boutra.nacir@gmail.com",
     packages=find_packages()
 )
